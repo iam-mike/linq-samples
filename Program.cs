5a49fae43f466d22d1bf202d9c44da74efd14fc2
Mike Witts <mike.witts@microsoft.com>
2021-05-20 20:37:12 +0100

    added linq query

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace linq_fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @".\";
+            ShowLargeFilesWithoutLinq(path);
+        }
+    }
+}
---
+++ linq-fundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linqfundamentals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
