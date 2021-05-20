337c8ddd18d67cf9f0ef233e053453e654a23f7b
Mike Witts <mike.witts@microsoft.com>
2021-05-20 21:40:03 +0100

    added basic linq

---
+++ features/MyLinq.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    public static class MyLinq
+    {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            var count = 0;
+            foreach (var item in sequence)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
