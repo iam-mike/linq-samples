eacb3bc3c66a73ddcb9482f3cd7bfdf92320ceca
Mike Witts <mike.witts@outlook.com>
2021-05-23 21:42:20 +0100

    filter, order, project

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 namespace Cars
@@ -16,16 +17,25 @@ namespace Cars
             var query2 = from car in cars
                          where car.Manufacturer == "BMW" && car.Year == 2016
                          orderby car.Combined descending, car.Name ascending
-                         select car;
+                         select new
+            {
+                car.Name,
+                car.Manufacturer,
+                car.Combined
+            };
 
-            var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                          .OrderByDescending(c => c.Combined)
-                          .ThenBy(c => c.Name)
-                          .Select(c => c)
-                          .First();
+            var result = cars.Select(c => c.Name);
+            //All
+            //Contains
 
-            Console.WriteLine(top.Name);
-                         
+            //var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+            //              .OrderByDescending(c => c.Combined)
+            //              .ThenBy(c => c.Name)
+            //              .Select(c => c)
+            //              .First();
+
+            Console.WriteLine(result);
+
             foreach (var car in query2.Take(10))
             {
                 Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
@@ -35,12 +45,42 @@ namespace Cars
         private static System.Collections.Generic.List<Car> ProcessFile(string path)
         {
             var query =
-                from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
+
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
+
+            //from line in File.ReadAllLines(path).Skip(1)
+            //where line.Length > 1
+            //select Car.ParseFromCsv(line);
 
 
-                return query.ToList();
+            return query.ToList();
         }
+    }
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+ 
+
     }
 }
